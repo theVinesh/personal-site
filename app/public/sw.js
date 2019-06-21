a262b8cc7f8aae89b1d1a3f82def2a3eea96f02f
Vinesh Raju <vineshraju@rocketmail.com>
2019-06-21 18:08:38 +0530

    v1.8
    * load font at the end
    
    * removes lighthouse integration
    
    * fix path erron in manifest
    
    * add lang to html tag
    
    * load font at the end
    
    * removes lighthouse integration
    
    * fix path erron in manifest
    
    * add lang to html tag
    
    * Reduces nesting in css and html dom
    
    * Adds hover effects
    
    * Bump up page-cache version
    
    * Update ci to minify html
    
    * remove caching based on branch
    
    * Removes unnecessary media query
    
    * Removes border radius on hover
    
    * Content change
    
    * Removes caching pages and console logs
    
    * Adds nav bar, footer and small ui changes
    
    * Adds Firebase Performance

--- app/public/sw.js
+++ app/public/sw.js
@@ -1,14 +1,13 @@
-const CACHE = "page-cache-v1";
+//Make sure to update this everytime there is change in ui
+// even better make it auto increment in ci/cd
+const CACHE = "page-cache-v1.7";
 
 const filesToCache = [
-  '/',
-  'style/main.css',
-  'index.html',
+  //TODO - cache this when most styling is done 'style/main.css',
   'https://fonts.googleapis.com/css?family=Oxygen+Mono&display=swap&&effect=anaglyph'
 ];
 
 self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
     caches.open(CACHE)
       .then(cache => {
@@ -18,39 +17,30 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then(response => {
         if (response) {
-          console.log('Found ', event.request.url, ' in cache');
           return response;
         }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request)
-
-          .then(response => {
-            // TODO 5 - Respond with custom 404 page
-            return caches.open(CACHE).then(cache => {
-              cache.put(event.request.url, response.clone());
-              return response;
-            });
-          });
-
-
+        return fetch(event.request);
+          // .then(response => {
+          //   // TODO - Respond with custom 404 page
+          //   return caches.open(CACHE).then(cache => {
+          //     cache.put(event.request.url, response.clone());
+          //     return response;
+          //   });
+          // });
       }).catch(error => {
 
-        // TODO 6 - Respond with custom offline page
+        // TODO - Respond with custom offline page
 
       })
   );
 });
 
 self.addEventListener('activate', event => {
-  console.log('Activating new service worker...');
-
   const cacheWhitelist = [CACHE];
-
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
