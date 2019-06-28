83ec8504fb45746c29963574b2edd8ca5b0c1407
Vinesh Raju <vineshraju@rocketmail.com>
2019-06-28 19:27:47 +0530

    Handles routing with service worker

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,9 +1,15 @@
+const routesMap = new Map([
+  ["\/", "\/index.html"],
+  ["\/home", "\/index.html"],
+]);
+const pageRouteMatcher = ({ url, event }) => { return routesMap.has(url.pathname); Î };
+const pageRouteHandler = async ({ url, event, params }) => { return fetch(routesMap.get(url.pathname) + url.search); };
+const router = workbox.routing;
+router.registerRoute(pageRouteMatcher, pageRouteHandler);
+router.setDefaultHandler(({ url, event, params }) => {
+  return fetch(event.request);
+});
+workbox.googleAnalytics.initialize();
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
-
-// workbox.routing.registerRoute(
-//   new RegExp('https://hacker-news.firebaseio.com'),
-//   new workbox.strategies.StaleWhileRevalidate()
-// );
-
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
