bdf8cfd8772a86d28bafd014b55225962cf8edcb
Vinesh Raju <vineshraju@rocketmail.com>
2019-06-28 17:13:28 +0530

    Use workbox to generate service-worker

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,57 +1,9 @@
-//Make sure to update this everytime there is change in ui
-// even better make it auto increment in ci/cd
-const CACHE = "page-cache-v1.8";
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
-const filesToCache = [
-  //TODO - cache this when most styling is done 'style/main.css',
-  'pages/404.html',
-  'https://fonts.googleapis.com/css?family=Oxygen+Mono&display=swap&&effect=anaglyph'
-];
+// workbox.routing.registerRoute(
+//   new RegExp('https://hacker-news.firebaseio.com'),
+//   new workbox.strategies.StaleWhileRevalidate()
+// );
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-          // .then(response => {
-          //   // TODO - Respond with custom 404 page
-          //   return caches.open(CACHE).then(cache => {
-          //     cache.put(event.request.url, response.clone());
-          //     return response;
-          //   });
-          // });
-      }).catch(error => {
-
-        // TODO - Respond with custom offline page
-
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const CssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest')
+const workboxPlugin = require('workbox-webpack-plugin');
 const fs = require('fs')
 
 function generateHtmlPlugins(templateDir) {
@@ -64,7 +65,7 @@ module.exports = {
                 ]
             },
             {
-                test: /(robots.txt|sw.js)$/,
+                test: /robots.txt$/,
                 use: [
                     {
                         loader: 'file-loader',
@@ -79,6 +80,10 @@ module.exports = {
     plugins: [
         new CssExtractPlugin({
             filename: 'style/main.css'
+        }),
+        new workboxPlugin.InjectManifest({
+            swSrc: './src/js/sw.js',
+            swDest: 'sw.js',
         }),
         new WebpackPwaManifest({
             name: "Vinesh Raju's Site",
