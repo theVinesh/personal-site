14c777ab8f5618c6da0fe70d178efcb39e593045
Vinesh Raju <vineshraju@rocketmail.com>
2019-07-05 21:11:24 +0530

    v2.4.3 (#30)
    * Removes sw routing and use firebase cleanUrls=true (#29)
    
    * Make ga global (#28)
    
    adding analytics.js manually. Should make it automatic

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
+import { Analytics } from '../js/analytics/base.js'
 require("../scss/main.scss");
 require("../assets/robots.txt");
 
-function init() {
+const analytics = new Analytics();
+
+const main = () => {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('/sw.js')
         });
     }
-}
+    
+};
 
-init();
+main();
