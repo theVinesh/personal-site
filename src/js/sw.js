9ee38214ef06d38452563611a3b9ee7fabbfde9e
Vinesh Raju <vineshraju@rocketmail.com>
2019-07-05 21:09:42 +0530

    Removes sw routing and use firebase cleanUrls=true (#29)

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,14 +1,3 @@
-const routesMap = new Map([
-  ["\/", "\/index.html"],
-  ["\/home", "\/index.html"],
-]);
-const pageRouteMatcher = ({ url, event }) => { return routesMap.has(url.pathname); Î };
-const pageRouteHandler = async ({ url, event, params }) => { return fetch(routesMap.get(url.pathname) + url.search); };
-const router = workbox.routing;
-router.registerRoute(pageRouteMatcher, pageRouteHandler);
-router.setDefaultHandler(({ url, event, params }) => {
-  return fetch(event.request);
-});
 workbox.googleAnalytics.initialize();
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
