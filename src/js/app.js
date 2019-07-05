fead76e6a66768cdd3c55ef998ab24d476fecc95
Vinesh Raju <vineshraju@rocketmail.com>
2019-07-06 02:18:19 +0530

    Integrates Firebase performance

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
+import {FirebaseUtil} from './firebase_util'
 import style from '../scss/main.scss'
 require("../assets/robots.txt");
+
+const firebaseUtil = new FirebaseUtil();
 const main = () => {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('/sw.js')
         });
     }
+    firebaseUtil.initPerformance();
 };
 main();
