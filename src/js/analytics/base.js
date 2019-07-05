d35c96e88c48a00fb1c5e480644c960c18dfc0d2
Vinesh Raju <vineshraju@rocketmail.com>
2019-07-06 02:25:28 +0530

    Removes Google Analytics and adds Firebse Performance (#34)
    * Removes Google Analytics
    
    * Integrates Firebase performance

--- src/js/analytics/base.js
+++
@@ -1,8 +0,0 @@
-export class Analytics{
-      init() {
-        window.ga = window.ga || ((...args) => (ga.q = ga.q || []).push(args));
-        ga('create', 'UA-142502015-1', 'auto');
-        ga('set', 'transport', 'beacon');
-        ga('send', 'pageview');
-     }
-}
--- src/js/sw.js
+++ src/js/sw.js
@@ -1,4 +1,3 @@
-workbox.googleAnalytics.initialize();
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 // workbox.precaching.precacheAndRoute(self.__precacheManifest);
