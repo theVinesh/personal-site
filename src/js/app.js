39455f3d98453974af2a58f881d315221f8e891a
Vinesh Raju <vineshraju@rocketmail.com>
2019-07-06 01:24:45 +0530

    Removes Google Analytics

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,10 @@
-import { Analytics } from '../js/analytics/base.js'
-require("../scss/main.scss");
+import style from '../scss/main.scss'
 require("../assets/robots.txt");
-
-const analytics = new Analytics();
-
 const main = () => {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('/sw.js')
         });
     }
-    
 };
-
 main();
