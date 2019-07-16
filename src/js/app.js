2397827f9810a928a5f24192e887780600071207
Vinesh Raju <vineshraju@rocketmail.com>
2019-07-16 21:26:02 +0530

    v2.4.9 (#51)
    * Adds Resume (#48)
    
    * Adds Resume
    
    * Adds styling to Resume
    
    * Hooks Resume to Home
    
    * Correct gramatical mistakes and remove firebase performance (#50)
    
    * Entire tab on nav bar clickable and grammar change
    
    * Updates babel
    
    * Removes robot.txt
    
    * Removes firebase performance
    
    * Adds better Description

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,11 @@
-import {FirebaseUtil} from './firebase_util'
 import style from '../scss/main.scss'
-require("../assets/robots.txt");
+// require("../assets/robots.txt");
 
-const firebaseUtil = new FirebaseUtil();
 const main = () => {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('/sw.js')
         });
     }
-    firebaseUtil.initPerformance();
 };
 main();
