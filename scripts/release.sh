113ce1174721b574a50a8a2a8c7ef84e746fe865
Vinesh Raju <vineshraju@rocketmail.com>
2019-08-23 19:45:41 +0530

    Fix auto-versioning bug

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-version_string=`git describe --abbrev=1 | sed 's/-/./g'`
-# split version_string
-IFS=. read -r major minor patch commit_hash<<<"${version_string:1}"
-
-if (( $patch > 9 )); then 
-    patch=0
-    minor=$((minor+1))
-fi
-
-if (( $minor > 9 )); then 
-    minor=0
-    major=$((major+1))
-fi
-
-export VERSION="v$major.$minor.$patch"
-echo "$VERSION"
-
-curl -X POST \
+bash scripts/version.sh && curl -X POST \
   https://api.github.com/repos/theVinesh/personal-site/releases \
   -H 'Accept: application/vnd.github.v3+json' \
   -H 'Authorization: token '$GITHUB_TOKEN'' \
@@ -28,4 +11,4 @@ curl -X POST \
   "name": "'$VERSION'",
   "draft": true
 }
-' > /dev/null
+'
