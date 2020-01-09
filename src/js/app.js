d9b71eb3495290a67315ada96ea883879b5249c9
Vinesh Raju <vineshraju@rocketmail.com>
2020-01-10 00:29:23 +0530

    Adds Dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ import style from '../scss/main.scss'
 // require("../assets/robots.txt");
 
 const main = () => {
+    document.addEventListener('DOMContentLoaded', (event) => { setTheme() });
+    document.getElementById("dark_mode_toggle").addEventListener("click", (event) =>{toggleDarkMode()});
+
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('/sw.js')
@@ -9,3 +12,12 @@ const main = () => {
     }
 };
 main();
+
+const toggleDarkMode = () => {
+    localStorage.setItem('mode', (localStorage.getItem('mode') || 'light') === 'dark' ? 'light' : 'dark');
+    setTheme();
+}
+
+const setTheme = () => {
+    localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark');
+};
