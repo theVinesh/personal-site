161795d94ecbc917a9f24ef98e3eaffd98fec463
theVinesh <hi@thevinesh.com>
2020-12-17 01:22:22 +0530

    Update metadata

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -3,19 +3,10 @@ import 'package:flutter/material.dart';
 class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      children: [
-        Container(
-          child: Center(
-            child: RichText(
-                text: TextSpan(
-                    style: Theme.of(context).textTheme.bodyText2,
-                    children: [
-                  TextSpan(text: "Developed with Flutter by Vinesh")
-                ])),
-          ),
-        ),
-      ],
+    return Container(
+      child: Center(
+        child: Text("2069, Vinesh"),
+      ),
     );
   }
 }
