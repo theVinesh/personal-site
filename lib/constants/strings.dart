d6e769c4e30562b27634789e66c8901ef5a6daa3
theVinesh <hi@thevinesh.com>
2020-12-17 19:26:18 +0530

    Remove emoji

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -3,16 +3,15 @@ class AppStrings {
   static final toggleDarkMode = "Toggle dark mode.";
   static final helloWorld = "hello world!";
   static final introText =
-      "I am a Mobile applications Designer / Developer from India 🇮🇳.\n" +
+      "I am a Mobile applications Designer / Developer from India.\n" +
           "I'm currently leading the development of Multipl's client application on both iOS and Android.\n";
   static final iAmVinesh = "I am Vinesh, ";
   static final whatDoIDo = [
-    "I make Mobile Apps 📱",
-    "I occasionally Cook 🍳",
-    "I make Web apps ‍💻",
-    "I love to Sing and whistle 😙🎶",
-    "I like to Sketch ✍️",
-    "I Party hard 🕺",
+    "I make Mobile Apps",
+    "I occasionally Cook",
+    "I make Web apps",
+    "I love to Sing and whistle",
+    "I like to Sketch",
   ];
 
   static const bgWordWall = "theVinesh. from Tirunelveli, Tamil Nadu, India."
